bf3116fb255cfbcc5f9df27ae30d76b7e6520eb6
Owen Iwamasa <owiwamasa@gmail.com>
2021-09-11 22:32:16 -0400

    adding media quiries

---
+++ src/components/DropDown/index.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import './DropDown.css'
+
+function DropDown ({scrollProjects, scrollSkills}) {
+    const [dropdown, setDropdown] = useState(false)
+
+    return(
+        <div className='dropdown'>
+            {!dropdown ?
+            <i onClick={() => setDropdown(true)} class="fas fa-bars"></i>
+            :
+            <div className='dropdown-clicked'>
+                <i onClick={() => setDropdown(false)} class="fas fa-bars"></i>
+                <div className='dropdown-options'>
+                    <div onClick={() => {
+                        setDropdown(false)
+                        scrollProjects()
+                    }} className='dropdown-link'>Projects</div>
+                    <div onClick={() => {
+                        setDropdown(false)
+                        scrollSkills()
+                    }} className='dropdown-link'>Skills</div>
+                    <a onClick={() => setDropdown(false)} href='mailto:owiwamasa@gmail.com' className='dropdown-email'>owiwamasa@gmail.com</a>
+                </div>
+            </div>
+            }
+        </div>
+    )
+}
+
+export default DropDown
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -6,6 +6,7 @@ import headshot from '../../skill-icons/headshot.png'
 import Scroll from 'react-scroll'
 import resume from '../../skill-icons/Iwamasa_Owen_Resume.pdf'
 import logo from '../../skill-icons/logo.png'
+import DropDown from '../DropDown'
 
 function HomePage(){
     const Element  = Scroll.Element;
@@ -29,6 +30,7 @@ function HomePage(){
     return(
         <div>
             <img className='logo' src={logo} alt='logo'/>
+            <DropDown scrollProjects={scrollProjects} scrollSkills={scrollSkills}/>
             <div className='nav'>
                 <div onClick={scrollProjects} className='nav-link'>Projects</div>
                 <div onClick={scrollSkills} className='nav-link'>Skills</div>
@@ -40,47 +42,40 @@ function HomePage(){
                     <div className='headshot'>
                         <img src={headshot} alt='head shot'/>
                     </div>
-                    {/* <div>owiwamasa@gmail.com</div> */}
                 </div>
-                {/* <div className='link-box'>
-                    <div className='link'>
-                        <a href='owiwamasa@gmail.com'>
-                            <i className="far fa-envelope" />
-                        </a>
+                <div className='link-buttons'>
+                    <div className='link-box'>
+                        <div className='link'>
+                            <a href={resume}>
+                                <i class="far fa-file"></i>
+                            </a>
+                        </div>
+                        <div className='link-label'>Resume</div>
                     </div>
-                    <div className='link-label'>Email</div>
-                </div> */}
-                <div className='link-box'>
-                    <div className='link'>
-                        <a href={resume}>
-                            <i class="far fa-file"></i>
-                        </a>
+                    <div className='link-box'>
+                        <div className='link'>
+                            <a href='https://www.linkedin.com/in/owen-iwamasa-6ab3a9166/'>
+                                <i className="fab fa-linkedin" />
+                            </a>
+                        </div>
+                        <div className='link-label'>LinkedIn</div>
                     </div>
-                    <div className='link-label'>Resume</div>
-                </div>
-                <div className='link-box'>
-                    <div className='link'>
-                        <a href='https://www.linkedin.com/in/owen-iwamasa-6ab3a9166/'>
-                            <i className="fab fa-linkedin" />
-                        </a>
+                    <div className='link-box'>
+                        <div className='link'>
+                            <a href='https://github.com/owiwamasa'>
+                                <i className="fab fa-github" />
+                            </a>
+                        </div>
+                        <div className='link-label'>GitHub</div>
                     </div>
-                    <div className='link-label'>LinkedIn</div>
-                </div>
-                <div className='link-box'>
-                    <div className='link'>
-                        <a href='https://github.com/owiwamasa'>
-                            <i className="fab fa-github" />
-                        </a>
+                    <div className='link-box'>
+                        <div className='link'>
+                            <a href='https://angel.co/u/owen-iwamasa'>
+                                <i class="fab fa-angellist"></i>
+                            </a>
+                        </div>
+                        <div className='link-label'>AngelList</div>
                     </div>
-                    <div className='link-label'>GitHub</div>
-                </div>
-                <div className='link-box'>
-                    <div className='link'>
-                        <a href='https://angel.co/u/owen-iwamasa'>
-                            <i class="fab fa-angellist"></i>
-                        </a>
-                    </div>
-                    <div className='link-label'>AngelList</div>
                 </div>
             </div>
             <Element name="scrollToSkills">
